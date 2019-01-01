db69149704e9aa079b45d7c3c85d78cce5376471
ScalingSun <timwilms@live.nl>
2019-01-01 14:49:51 +0100

    Add files via upload

---
+++ iedereenkanschilderen/iedereenkanschilderen/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace iedereenkanschilderen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.txbContent = new System.Windows.Forms.TextBox();
+            this.btnNewFile = new System.Windows.Forms.Button();
+            this.txbNameFile = new System.Windows.Forms.TextBox();
+            this.lblFileOpened = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txbSaveFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(267, 41);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(267, 143);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 3;
+            this.btnDraw.Text = "Draw!";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            // 
+            // txbContent
+            // 
+            this.txbContent.Location = new System.Drawing.Point(12, 41);
+            this.txbContent.Multiline = true;
+            this.txbContent.Name = "txbContent";
+            this.txbContent.Size = new System.Drawing.Size(239, 387);
+            this.txbContent.TabIndex = 4;
+            // 
+            // btnNewFile
+            // 
+            this.btnNewFile.Location = new System.Drawing.Point(373, 12);
+            this.btnNewFile.Name = "btnNewFile";
+            this.btnNewFile.Size = new System.Drawing.Size(75, 23);
+            this.btnNewFile.TabIndex = 5;
+            this.btnNewFile.Text = "New File";
+            this.btnNewFile.UseVisualStyleBackColor = true;
+            this.btnNewFile.Click += new System.EventHandler(this.btnNewFile_Click);
+            // 
+            // txbNameFile
+            // 
+            this.txbNameFile.Location = new System.Drawing.Point(267, 14);
+            this.txbNameFile.Name = "txbNameFile";
+            this.txbNameFile.Size = new System.Drawing.Size(100, 20);
+            this.txbNameFile.TabIndex = 6;
+            this.txbNameFile.Text = "Filename";
+            // 
+            // lblFileOpened
+            // 
+            this.lblFileOpened.AutoSize = true;
+            this.lblFileOpened.Location = new System.Drawing.Point(13, 14);
+            this.lblFileOpened.Name = "lblFileOpened";
+            this.lblFileOpened.Size = new System.Drawing.Size(68, 13);
+            this.lblFileOpened.TabIndex = 7;
+            this.lblFileOpened.Text = "File opened: ";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(373, 69);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save file";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txbSaveFileName
+            // 
+            this.txbSaveFileName.Location = new System.Drawing.Point(267, 70);
+            this.txbSaveFileName.Name = "txbSaveFileName";
+            this.txbSaveFileName.Size = new System.Drawing.Size(100, 20);
+            this.txbSaveFileName.TabIndex = 9;
+            this.txbSaveFileName.Text = "Filename";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 450);
+            this.Controls.Add(this.txbSaveFileName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblFileOpened);
+            this.Controls.Add(this.txbNameFile);
+            this.Controls.Add(this.btnNewFile);
+            this.Controls.Add(this.txbContent);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnLoadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TextBox txbContent;
+        private System.Windows.Forms.Button btnNewFile;
+        private System.Windows.Forms.TextBox txbNameFile;
+        private System.Windows.Forms.Label lblFileOpened;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txbSaveFileName;
+    }
+}
+
---
+++ iedereenkanschilderen/iedereenkanschilderen/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace iedereenkanschilderen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void btnNewFile_Click(object sender, EventArgs e)
+        {
+            Createfile();
+            MessageBox.Show("succesvol gecreeerd....");
+            OpenFile();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void SaveFile()
+        {
+            string filename = txbSaveFileName.Text + ".txt";
+
+            // Write each directory name to a file.
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                string content = txbContent.Text;
+                sw.Write(content);
+            }
+
+            /*string content = txbContent.Text;
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();   
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog1.Title = "Save File";
+                saveFileDialog1.ShowDialog();
+            banaan
+                if (saveFileDialog1.FileName != "")
+                {
+                FileStream fs = (FileStream)saveFileDialog1.OpenFile();
+            }*/
+        }
+        private void Createfile()
+        {
+            using (FolderBrowserDialog Browse = new FolderBrowserDialog())
+            {
+                Browse.ShowDialog();
+                string path = Browse.SelectedPath;
+                string file = "\\" + txbNameFile.Text + ".txt";
+                string pathfile = path + file;
+                using (FileStream fs = File.Create(pathfile))
+                {
+                    //Byte[] info = new UTF8Encoding(true).GetBytes("");
+                    // Add some information to the file.
+                    //fs.Write(info, 0, info.Length);
+                }
+            }
+        }
+        private void OpenFile()
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file    
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        txbContent.Text = fileContent;
+
+                    }
+                }
+            }
+        }
+    }
+
+}
